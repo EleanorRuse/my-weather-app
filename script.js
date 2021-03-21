3b30c021e743a1366f7d63d2ec68e510fc83b9b9
Eleanor <eleanor.ruse@hotmail.co.uk>
2021-03-21 21:35:44 +0000

    Tidying Up Code

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-//Set up to automatically update page with current location
-
-//API Key 
+//API Key & unit 
 let apiKey = "99249e6036b7cd3ba4446e3f8c097e60";
 let units = "metric";
 
+//Search Bar 
+function search(city){
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayForecast);
+}
+ 
+function handleSubmit(event){
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#search-text-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+
+//date and time
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let days = [
@@ -32,6 +50,7 @@ function formatHours(timestamp) {
   return `${hours}:${minutes}`;
 }
 
+//Temperature Display 
 function displayTemperature(response) {
   responseTemperature = response
   let temperatureElement = document.querySelector("#temperature");
@@ -45,7 +64,7 @@ function displayTemperature(response) {
   let maxElement = document.querySelector("#maxTemp");
   let minElement = document.querySelector("#minTemp");
 
-
+//Celsius and Fahrenhet for current weather
   let celsiusTemperature = response.data.main.temp;
   let feelsLike = response.data.main.feels_like;
 
@@ -59,7 +78,7 @@ function displayTemperature(response) {
 
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
-  visibilityElement.innerHTML = response.data.visibility / 100;
+  visibilityElement.innerHTML = response.data.visibility/100;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
@@ -70,6 +89,58 @@ function displayTemperature(response) {
   currentEmoji.setAttribute ("alt",response.data.weather[0].description);
 }
 
+//Current Location Data API
+
+function showcurrentLocation(position) {
+  let cityInputElement = document.querySelector("#search-text-input");
+  cityInputElement.value = "";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function getCurrentPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showcurrentLocation);
+}
+
+let currentButton = document.querySelector("#location-search");
+currentButton.addEventListener("click", getCurrentPosition);
+
+search("London");
+
+//Fahrenheit and Celsius Display 
+function displayFahrenheitTemperature(event){
+  event.preventDefault();
+  selectedUnit = "F";
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+
+  displayTemperature(responseTemperature);
+  displayForecast(responseForecast);
+}
+
+function displayCelsiusTemperature(event){
+  event.preventDefault();
+  selectedUnit = "C";
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+
+  displayTemperature(responseTemperature);
+  displayForecast(responseForecast);
+}
+
+let fahrenheitLink = document.querySelector("#fahrenhiet-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+let selectedUnit = "C";
+
+//Forecast weather
 function displayForecast(response){
   responseForecast = response
   let forecastElement = document.querySelector("#forecast");
@@ -103,71 +174,3 @@ function displayForecast(response){
 `;
   } 
 }
-
-function search(city){
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(displayTemperature);
-
-  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(displayForecast);
-}
- 
-function handleSubmit(event){
-  event.preventDefault();
-  let cityInputElement = document.querySelector("#search-text-input");
-  search(cityInputElement.value);
-}
-
-function displayFahrenheitTemperature(event){
-  event.preventDefault();
-  selectedUnit = "F";
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-
-  displayTemperature(responseTemperature);
-  displayForecast(responseForecast);
-}
-
-function displayCelsiusTemperature(event){
-  event.preventDefault();
-  selectedUnit = "C";
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-
-  displayTemperature(responseTemperature);
-  displayForecast(responseForecast);
-}
-
-function showcurrentLocation(position) {
-  let cityInputElement = document.querySelector("#search-text-input");
-  cityInputElement.value = "";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(displayTemperature);
-
-  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(displayForecast);
-}
-
-function getCurrentPosition(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(showcurrentLocation);
-}
-
-let selectedUnit = "C";
-
-let responseTemperature = null;
-let responseForecast = null;
-
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", handleSubmit);
-
-let fahrenheitLink = document.querySelector("#fahrenhiet-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
-
-let currentButton = document.querySelector("#location-search");
-currentButton.addEventListener("click", getCurrentPosition);
-
-search("London");
